ec783f9fec10bfec31ca8966eab8d5a132cb3a7d
Reeyth <78480777+Reeyth@users.noreply.github.com>
2023-04-30 23:33:19 +0200

    Game page

--- uin-exam/src/components/Game.js
+++ uin-exam/src/components/Game.js
@@ -19,12 +19,32 @@ export default function Game() {
   }, [slug]);
 
   return (
-    <div className="text-xl">
-      <h1>Game</h1>
+		<div className="text-xl text-center">
+      <div className="font-semibold text-3xl mb-3">{game.title}</div>
+			<img src={game.image} alt={game.title} className="rounded-t-xl mx-auto mb-5" />
+			<div className="mb-5">
+				{game.categories?.map((category) => (
+					<span key={category.title} className="mr-2 text-blue-700 bg-cyan-300 p-1 rounded bg-opacity-50" >
+						{category.title}
+					</span>
+				)) || "No categories found"}
+			</div>
+      <div className="mb-100">
+        <div>Release Date: {game.releaseDate}</div>
+        <div>Developers: {game.developers}</div>
+        <div>Publishers: {game.publisher}</div>
+        <div>Platforms: {game.platforms}</div>
+        <div>Rating: {game.rating}/5</div>
+        <div>Stores: {game.stores}</div>
+      </div>
 
-      <pre>
-        <code>{JSON.stringify(game, null, 2)}</code>
-      </pre>
-    </div>
-  );
+      <p className="text-2xl mt-10 mb-2">Summary</p>
+			<div className="text-gray-800 text-left w-6/12 mx-auto">{game.summary}</div>
+      <p className="text-2xl mt-10 mb-2">Description</p>
+			<div className="text-gray-800 text-left w-6/12 mx-auto" >{game.description}</div>
+		  {/* <pre>
+				<code>{JSON.stringify(game, null, 2)}</code>
+			</pre>  */}
+		</div>
+	);
 }
