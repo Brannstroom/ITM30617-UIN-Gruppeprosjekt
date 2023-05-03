fd00db6595dbeb93898e3ab17f2405836f7bd773
UllernJ <Ulrik.johannesen@gmail.com>
2023-05-03 16:04:26 +0200

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,6 +15,7 @@ export default function Game() {
         setTags(game.tags.map((tag) => {
           return {value: tag?.name, count: tag?.games_count}
         }));
+        console.log(game)
       })
         .catch((error) => {
         console.log(error);
@@ -22,24 +23,30 @@ export default function Game() {
   }, [slug]);
 
   return (
-    
+
+
     <div>
-      <div className="w-1/2">
-        <img src={game.background_image} alt={game.name} />
-      </div>
-      <div className="text-2xl">
-        <h1>{game.name}</h1>
+      {/*Make a nice overview for a game, use tailwind for css styling **/}
+        <div className="flex flex-row">
+            <div className="flex flex-col">
+                <h1 className="text-3xl font-bold">{game.name}</h1>
+                <p className="text-xl">{game.summary}</p>
+                <img src={game.background_image} alt={game.name} className="w-1/4"/>
+                <p className="text-xl">Rating: {game.rating}</p>
+                <p className="text-xl">Release date: {game.released}</p>
+                <p className="text-xl">Genres: {game.genres?.map((genre) => genre.name).join(", ")}</p>
+                <p className="text-xl">Plot: {game.description_raw}</p>
+                <p className="text-xl">Platforms: {game.parent_platforms?.map((platform) => platform.platform.name).join(", ")}</p>
+                <p className="text-xl">Developers: {game.developers?.map((dev) => dev.name).join(", ")}</p>
+                <p className="text-xl">Publishers: {game.publishers?.map((publisher) => publisher.name).join(", ")}</p>
+                <p className="text-xl">Stores: {game.stores?.map((store) => store.store.name).join(", ")}</p>
+            </div>
         </div>
-        <div className="text-2xl">
-        <h2>{game.released}</h2>
-        </div>
-        <div className="text-2xl">
-        <h3>{game.genres?.map((genre) => genre.name).join(", ")}</h3>
-        </div>
-        <div className="text-2xl">
-        <h4>Rating: {game.rating}</h4>
-        </div>
-        <TagCloud minSize={12} maxSize={35} tags={tags} />
+
+
+
+
+        <TagCloud minSize={15} maxSize={35} tags={tags} />
 
 
     </div>
