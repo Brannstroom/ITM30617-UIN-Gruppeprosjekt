1d24ae27df39dc0d9971640154aa78f3a9497b6c
UllernJ <Ulrik.johannesen@gmail.com>
2023-05-10 00:07:12 +0200

    Update client.js

--- src/client.js
+++ src/client.js
@@ -4,6 +4,6 @@ export default createClient({
     projectId: "pz9qwh5p",
     dataset: "production",
     apiVersion: "2023-04-25",
-    useCdn: true,
+    useCdn: false,
     token: 'skCeNrOzbR8I3YfCXsAzaDllw0DxDtSvaKHCJZUMm1jyuR8eGqpFgRzxbmR4Oo7pILuDfWxGfO8dh9s8sn1NgJ52P1AMJyn7lQKhd2gwisUwo32rt9vsgNpXorDmfEtCyfaGk8cJdWumur6AZV1RpJSTapVqZzpOJ2IWQYlilhW0lz0wKrEs',
 })
