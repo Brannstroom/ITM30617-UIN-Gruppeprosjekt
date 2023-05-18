6cbdcc86b0abf84cd94263aedab6bdcbfc5e808b
UllernJ <Ulrik.johannesen@gmail.com>
2023-05-18 19:41:58 +0200

    Added responsive css

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { getFavoriteGames } from "../api/game";
+import {getFavoriteGames, getOwnedGamesByUser} from "../api/game";
 import { isUserLoggedIn } from "../utils/login";
 
 export default function Favorites() {
 	const [favorites, setFavorites] = useState([]);
-
+	const [ownedGames, setOwnedGames] = useState([]);
 	useEffect(() => {
 		if (!isUserLoggedIn()) {
 			window.location.href = "/login";
@@ -13,40 +13,40 @@ export default function Favorites() {
 		getFavoriteGames().then((favorites) => {
 			setFavorites(favorites);
 		});
+		getOwnedGamesByUser().then((ownedGames) => {
+			setOwnedGames(ownedGames);
+		});
 	}, []);
 
+	const isOwned = (game) => {
+		return ownedGames.some((ownedGame) => ownedGame.id === game.id);
+	}
+
 	return (
-		<div>
-			<div className="text-center">
-				<h1 className="text-3xl font-semibold tracking-wide mt-2">
-					My Favorites
-				</h1>
-				<h2 className="text-lg text-gray-600">
-					You have {favorites.length} {favorites.length > 1 ? " favorite games" : "favorite game"}
-				</h2>
-			</div>
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 mt-4">
-				{favorites.map((game) => (
-					<article key={game.id} className="bg-white rounded-lg shadow-md">
-						<a href={`/game/${game.slug}`} className="block">
+		<>
+			<section className="grid grid-cols-5 gap-4 mt-10">
+				{favorites
+					.sort((a, b) => b.playtime - a.playtime)
+					.map((game) => (
+						<a
+							key={game.id}
+							href={`/game/${game.slug}`}
+							className="flex flex-col bg-white rounded-xl shadow-md overflow-hidden">
 							<img
 								src={game.background_image}
 								alt={game.name}
-								className="w-full h-64 object-cover"
-							/>
+								className="w-full h-48 object-cover"/>
+							<div className="p-4">
+								<div className="font-semibold text-lg mb-2">{game.name}</div>
+								{isOwned(game) && (
+									<div className="text-gray-600">
+										Hours Played: {game.playtime}
+									</div>
+								)}
+							</div>
 						</a>
-						<div className="p-4">
-							<a
-								href={`/game/${game.slug}`}
-								className="block text-black-500 hover:text-gray:600 font-semibold text-lg mb-2"
-							>
-								{game.name}
-							</a>
-							<div className="text-gray-600">Time played: {game.playtime}</div>
-						</div>
-					</article>
-				))}
-			</div>
-		</div>
+					))}
+			</section>
+		</>
 	);
 }
--- src/components/Navigation/Sidebar.js
+++ src/components/Navigation/Sidebar.js
@@ -22,58 +22,54 @@ const Sidebar = () => {
   const LinkStyleSub = "ml-8 hover:text-gray-400 mt-2";
 
   return (
-    <div className="fixed top-0 left-0 h-full w-72 bg-gray-800">
-      <div className="border-b border-gray-700 h-14 items-center flex">
-        <a href="/" className="w-11/12 m-auto">
-        <img src={logo} alt="logo" className="w-11/12 m-auto" />
-        </a>
-      </div>
-
-      <nav className="flex flex-col flex-1 mt-4 text-2xl text-gray-200  w-4/6 m-auto">
-        <a href="/" className={linkStyles}>
-          Home
-        </a>
-
-        <div className="mt-5 ml-4">
-          <a href="/games" className="hover:text-gray-400">
-            Store
+      <div className="fixed top-0 left-0 h-full w-72 bg-gray-800">
+        <div className="border-b border-gray-700 h-14 flex items-center">
+          <a href="/" className="w-11/12 m-auto">
+            <img src={logo} alt="logo" className="w-11/12 m-auto" />
           </a>
-          <button
-            onClick={() => setStoreDropDown(!storeDropDown)}
-            className="w-20 hover:text-gray-400"
-          >
-            {storeDropDown ? <ChevronDown /> : <ChevronRight />}
-          </button>
         </div>
 
-        {storeDropDown &&
-          categories &&
-          categories.map((category) => (
-            <a
-              href={`/games/${category.toLowerCase()}`}
-              key={category}
-              className={LinkStyleSub}
-            >
-              {category}
+        <nav className="flex flex-col flex-1 mt-4 text-2xl text-gray-200">
+          <a href="/" className={linkStyles}>
+            Home
+          </a>
+
+          <div className="mt-5 ml-4">
+            <a href="/games" className="hover:text-gray-400">
+              Store
             </a>
-          ))}
-      {isUserLoggedIn() ? (
-        <>
-        <a href="/library" className={linkStyles}>
-        Game library
-        </a>
-        <a href="/favorites" className={linkStyles}>
-        Favorites
-        </a>
-        </>
-      ) :  null}
+            <button
+                onClick={() => setStoreDropDown(!storeDropDown)}
+                className="w-20 hover:text-gray-400"
+            >
+              {storeDropDown ? <ChevronDown /> : <ChevronRight />}
+            </button>
+          </div>
 
-
-      </nav>
-
-
-      <LoginCard />
-    </div>
+          {storeDropDown &&
+              categories &&
+              categories.map((category) => (
+                  <a
+                      href={`/games/${category.toLowerCase()}`}
+                      key={category}
+                      className={LinkStyleSub}
+                  >
+                    {category}
+                  </a>
+              ))}
+          {isUserLoggedIn() && (
+              <>
+                <a href="/library" className={linkStyles}>
+                  Game library
+                </a>
+                <a href="/favorites" className={linkStyles}>
+                  Favorites
+                </a>
+              </>
+          )}
+        </nav>
+        <LoginCard />
+      </div>
   );
 };
 
