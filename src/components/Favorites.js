d7be63d8a0dfc72792aed0caf1bddca93a237701
Reeyth <78480777+Reeyth@users.noreply.github.com>
2023-05-19 10:36:54 +0200

    Header in favorites

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -20,10 +20,18 @@ export default function Favorites() {
 
 	const isOwned = (game) => {
 		return ownedGames.some((ownedGame) => ownedGame.id === game.id);
-	}
+	};
 
 	return (
 		<>
+			<div className="text-center">
+				<h1 className="text-3xl font-semibold tracking-wide mt-2">
+					My Favorites
+				</h1>
+				<h2 className="text-lg text-gray-600">
+					You have {favorites.length} {favorites.length > 1 ? "favorites" : "favorite"}
+				</h2>
+			</div>
 			<section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-10">
 				{favorites
 					.sort((a, b) => b.playtime - a.playtime)
@@ -31,7 +39,8 @@ export default function Favorites() {
 						<a
 							key={game.id}
 							href={`/game/${game.slug}`}
-							className="flex flex-col bg-white rounded-xl shadow-md overflow-hidden">
+							className="flex flex-col bg-white rounded-xl shadow-md overflow-hidden"
+						>
 							<img
 								src={game.background_image}
 								alt={game.name}
